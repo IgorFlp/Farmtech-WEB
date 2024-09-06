c55c2d7c2196f75f5f0ddea04b3141eb36eb6e14
IgorFlp <igor.felipe.13@hotmail.com>
2024-09-05 22:53:24 -0300

    Stash

--- Models/DbContext.cs
+++ Models/DbContext.cs
@@ -12,9 +12,5 @@ namespace Farmtech_WEB.Models
         // Defina as tabelas do banco de dados como DbSet<>
         public DbSet<Venda> Vendas { get; set; }
         public DbSet<VendaProdutos> VendaProdutos { get; set; }
-
-
-
-
     }
 }
