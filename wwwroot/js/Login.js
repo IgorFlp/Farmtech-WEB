960e234aa927a59e4c928327927544f9effd4898
IgorFlp <igor.felipe.13@hotmail.com>
2024-09-13 19:58:51 -0300

    Atualiza branch

---
+++ wwwroot/js/Login.js
@@ -0,0 +1,13 @@
+﻿document.querySelector("#btnEntrar").addEventListener("click", () => {
+    const usuario = document.querySelector("#usuario").value;
+    const senha = document.querySelector("#senha").value;
+    console.log("Usuario: " + usuario + "Senha: " + senha);
+
+    //fetch pro banco de dados
+    if (usuario == "Jorge" && senha == "123") {
+        
+        location.replace("home.html");
+    } else {
+        alert("Login incorreto");
+    }
+});
